4609878dcc28dc14f54e0dc73268b5f618f658fd
snehalsutexdiv5-ux <snehalsutexdiv5@gmail.com>
2026-01-23 13:37:31 +0530

    loop.js

---
+++ loop.js
@@ -0,0 +1,88 @@
+// loops --repeating code bolcks
+// 1 1 1 1 1 1
+// 1 2 3 4 5 6
+
+// for loop 
+// why use for loop? when you knoe many times you want to repest a block of code.
+// print 1 to 10 --> console.log(1); console.log(2); ..... console.log(10);
+// kaya thi javnu che ---> kya sudhi javnu che --->  kevi rite javnu che
+// for (ex. 1 -> 50 -> increment by 1)
+// (start; end; change)
+for (let i= 1; i <= 10; i++){
+    console.log(i)
+}
+
+for ( let j = 9; j >= 1; j--){
+    console.log(j)
+}
+
+// "Hello"
+for (let i = 0; i < 5; i++) {
+ console.log("Hello")
+ }
+
+ for (let i= 21; i <= 30; i++){
+    console.log(i)
+}
+
+for ( let j = 91; j >= 81; j--){
+    console.log(j)
+}
+
+// "Hello"
+for (let i = 0; i < 20; i++) {
+   document.writeln("Snehal thummar")
+ }
+
+ // while loop --- do while loop
+ // kya thi javnu che --> kya rokavanu che ---> kavi rite javnu che
+ // while (ex. 1 -> hello world -> stop when condition false)
+ // start
+ // while(end){
+ // code
+ // change
+ // }
+
+//  let K = 5;
+//  while (K <=20){
+//     console.log("k with while loop:", K);
+//     K++;
+// }
+
+// convert into true
+
+// let k = 50;
+// while (k >= 20) {
+//     console.log(k);
+//     k--;
+// }
+
+// let b = 20;
+// while (b <= 60) {
+//     console.log(b);
+//     b++;
+// }
+
+let c = 30;
+while (c > 20) {
+    console.log(c);
+    c--;
+}
+
+let d = 0;
+while (d < 5) {
+    console.log("sneha thummar");
+    d++;
+}
+
+let f = 50;
+while (f <= 60) {
+    console.log(f);
+    f++;   
+}
+
+let q = 50;
+while (q <= 60) {
+    console.log(q);
+    q++;
+}
