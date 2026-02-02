61732d38cb05eeea91e46429ad95d218361721ea
snehalsutexdiv5-ux <snehalsutexdiv5@gmail.com>
2026-02-02 11:50:27 +0530

    edited loop.js

--- loop.js
+++ loop.js
@@ -8,31 +8,31 @@
 // kaya thi javnu che ---> kya sudhi javnu che --->  kevi rite javnu che
 // for (ex. 1 -> 50 -> increment by 1)
 // (start; end; change)
-for (let i= 1; i <= 10; i++){
-    console.log(i)
-}
+// for (let i= 1; i <= 10; i++){
+//     console.log(i)
+// }
 
-for ( let j = 9; j >= 1; j--){
-    console.log(j)
-}
+// for ( let j = 9; j >= 1; j--){
+//     console.log(j)
+// }
 
-// "Hello"
-for (let i = 0; i < 5; i++) {
- console.log("Hello")
- }
+// // "Hello"
+// for (let i = 0; i < 5; i++) {
+//  console.log("Hello")
+//  }
 
- for (let i= 21; i <= 30; i++){
-    console.log(i)
-}
+//  for (let i= 21; i <= 30; i++){
+//     console.log(i)
+// }
 
-for ( let j = 91; j >= 81; j--){
-    console.log(j)
-}
+// for ( let j = 91; j >= 81; j--){
+//     console.log(j)
+// }
 
-// "Hello"
-for (let i = 0; i < 20; i++) {
-   document.writeln("Snehal thummar")
- }
+// // "Hello"
+// for (let i = 0; i < 20; i++) {
+//    document.writeln("Snehal thummar")
+//  }
 
  // while loop --- do while loop
  // kya thi javnu che --> kya rokavanu che ---> kavi rite javnu che
@@ -63,26 +63,105 @@ for (let i = 0; i < 20; i++) {
 //     b++;
 // }
 
-let c = 30;
-while (c > 20) {
-    console.log(c);
-    c--;
-}
+// let c = 30;
+// while (c > 20) {
+//     console.log(c);
+//     c--;
+// }
 
-let d = 0;
-while (d < 5) {
-    console.log("sneha thummar");
-    d++;
-}
+// let d = 0;
+// while (d < 5) {
+//     console.log("sneha thummar");
+//     d++;
+// }
 
-let f = 50;
-while (f <= 60) {
-    console.log(f);
-    f++;   
-}
+// let f = 50;
+// while (f <= 60) {
+//     console.log(f);
+//     f++;   
+// }
 
-let q = 50;
-while (q <= 60) {
-    console.log(q);
-    q++;
-}
+// let q = 50;
+// while (q <= 60) {
+//     console.log(q);
+//     q++;
+// }
+
+
+// let g = 1;
+// while (g <= 20) {
+//   console.log("Hello");
+//   g++;
+// }
+
+// let h = 50;
+// while (h <= 60) {
+//   console.log(h);
+//   h++;
+// }
+
+// let q = 60;
+// while (q >= 50) {
+//   console.log(q);
+//   q--;
+// }
+
+// do while loop
+// do {} while (end);
+// start
+// do {
+// code
+// change}
+// while (end);}
+let j = 12 ; // start
+do {
+    console.log("do while loop:",j); // code
+    j++;   // change (condition)
+}  while (j < 20); // while check after executing code blcok (end)
+
+let z= 15 ; // start
+do {
+    console.log("do while loop:",z); // code
+    z++;   // change (condition)
+}  while (z < 10); 
+// why : answer is 15 otherwise 15 is more than to 10
+
+// break
+ for (let k= 1; k <= 201; k++) {
+    console.log("Loop with break",k);
+    if (k === 24){
+        break;
+    }
+ }
+
+let a=3;
+do{
+    console.log("do while loop",a);
+    a++;
+    }    while(a<20);
+
+
+    for(let t=1;t <=200;t++){
+        console.log("monsson with brack",t)
+        if(t === 20){
+            break;
+        }
+
+    }
+
+    // continue
+// for ( let a = 1; a <= 10; a++){
+//     if ( a===5){
+//         continue;
+//     }
+//     console.log(a);
+// }
+ 
+// for ( let A = 1; A <= 10; A++){
+//     if ( A >= 5){
+//         continue;
+//     }
+//     console.log("loop that skip number",A);
+// }
+
+ 
