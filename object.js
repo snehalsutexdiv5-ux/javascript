749b8054c1f8736b07464ce42f7abbf380a1cb87
snehalsutexdiv5-ux <snehalsutexdiv5@gmail.com>
2026-02-14 12:08:22 +0530

    object

--- object.js
+++ object.js
@@ -37,3 +37,145 @@ User['name'];
 // --> output: abc
 
 const step = [1000, 2000, 3000];
+
+// Nesting and Deep Access
+const user1 = {
+  name: "test",
+  address: {
+    city: "Surat",
+    pin: 395009,
+    locations: {
+      lat: 24.2,
+      lng: 77.4,
+    },
+  },
+  email: "test@gmail.com",
+}; // -- nested Object
+
+user1.address.locations.lat; // --> Deep Access
+// user1.address.location.lng;
+
+// Object destructuring:
+let { lat, lng } = user1.address.locations;
+let { name, email, address } = user1;
+let { city } = user1.address;
+
+let { lat: abc_lat } = user1.address.locations;
+// destructure and rename variable name
+
+let demo = {
+  first_name: "Demo",
+  last_name: "User",
+  age: 45,
+  email: "demo@user.com",
+};
+
+// Looping (for-in loop)
+// for (variable_name in object_name){}
+for (const key in demo) {
+  // console.log(key);
+  // obj.key --> not working
+  // console.log(demo[key]);
+  console.log(key, ":", demo[key]); // -- log whole object
+}
+
+// Copying Objects: Spred, Object.assign, deep-clone
+// Spred
+// let user_data = {...demo};
+let user_data = { gender: "Male", role: "user", ...demo }; // add key-value and copy object
+// Use case: State update
+const user = {
+  name: "Amit",
+  age: 30
+}
+
+const updatedUser = {
+  ...user,
+  age: 31
+};
+console.log(updatedUser);
+
+// Object.assign --> copy and add keys-value
+let user_data_assign = Object.assign({}, demo); // - nothing will be add just copy
+let user_data_assign1 = Object.assign({ phone_number: 7894567894 }, demo); // - add and copy
+// Use case: Default settings + user settings merge
+// const defaultSettings = {
+//   theme: "light",
+//   language: "en"
+// };
+
+// const userSettings = {
+//   theme: "dark"
+// };
+
+// const finalSettings = Object.assign({}, defaultSettings, userSettings);
+// console.log(finalSettings);
+
+// Object.assign vs spred
+// Object.assign() -- Method -- ES5 old version, Less used
+// Spred -- Operator -- State updated, most used in react application, clean code
+
+// deep clone
+let temp_user1_clone = {...user1}
+
+// user1_clone.address.city = "Valsad" --> that will change value of user1 too
+// issue : when you clone & copy nested object using spred or object.assing into new
+// variable that copy all value but nested object pass only refrence not copy
+// (Jyare tame nested object(parent obj.) spred na upyog thi copy karo tyare nested obj.
+// (child obj.) refrence pass karshe -- value copy thase nahi)
+
+// How to clone that -- use deep clone
+// first convert your object to string
+// JSON - JavaScript Object Notation
+// JSON.stringify(obj_name) -- convert object into string
+// JSON.parse(stingify_Object) -- convet string to object
+
+let clone_user1 = JSON.parse(JSON.stringify(user1))
+
+// Deep clone = completely new memory
+// use case:
+// Real time use:
+// API response handling
+// Avoid data mutation bugs
+
+// Object.entries -- add key and value into array formate
+// give array of arrays
+let key_value_arr = Object.entries(demo);
+
+// Use case: Display user details dynamically (UI)
+// ex. backend give you data into object formate
+
+// const user3 = {
+//   name: "Priya",
+//   city: "Ahmedabad",
+//   role: "Admin"
+// };
+
+// print key and value both in profile page
+// Object.entries(user3).forEach(([key, value]) => {
+//   console.log(`${key} : ${value}`);
+// });
+
+// Object.entries(user) ==> convert into Arrays
+// forEach ==> Array loop
+
+// Optional Chaining, Computed Properties
+
+// user1.address.location.lng;
+user1?.address?.location?.lng; // -> ? Optional Chaining
+
+// use case:
+// Without optional chaining → app crash
+// Backend data incomplete
+// API response safety
+
+// const apiResponse = {
+//   user: {
+//     profile: {
+//       name: "Neha"
+//     }
+//   }
+// };
+
+// console.log(apiResponse.user?.profile?.name);
+// console.log(apiResponse.user?.address?.city);
