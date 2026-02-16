5b508fc0aba6a6163b04b742d23d881b14a0d556
snehalsutexdiv5-ux <snehalsutexdiv5@gmail.com>
2026-02-16 12:20:17 +0530

    append and remove

--- dom.js
+++ dom.js
@@ -1,12 +1,10 @@
-// DOM -- Document Object Model
-// All Website Code --> DOM
-// Website Output --> DOM
+// dom -- document object model
+// all website code ---> DOM
+// website output ---> DOM
 // DOM -- HTML body
 
-// DOM Tree Structure:
-// Node --> Element (ex. h1, p, b, li), text (ex. lorem), comment
-
-// DOM Manipulation – change body
+// DOM tree structure:
+// node ---> Element (ex. h1, p, b, i), text (ex,lorem), comment
 // select element from html
 // change text
 // change html
@@ -14,59 +12,113 @@
 // change attribute
 // event listeners
 
-// Selecting Elements
-// getElementById
-let a = document.getElementById("abc");
+// select element
+// GetElementById
+let a = document.getElementById("abc")
 console.log(a);
-// console.dir(a);
 
-// getElementsByClassName
-let b = document.getElementsByClassName("abc");
+// getElementByClassName
+let b = document.getElementsByClassName("abc")
 console.log(b);
 
 // getElementsByTagName
-let temp_c = document.getElementsByTagName("h1");
-console.log(temp_c);
-
-// querySelector and querySelectorAll
-// most used in project and used by company
-let c = document.querySelector("h1"); // only select first element that find in your html
+let c = document.getElementsByTagName("h1")
 console.log(c);
 
-let d = document.querySelectorAll("h1"); // select all elements in your html
-console.log(d);
-// Text/Content access: innerText, textContent, innerHTML
-let e = document.querySelector("h1")
-console.dir(e);
+// querySelector --> only select first (tag , class)in html
+let d = document.querySelector(".abc");
 
-e.innerText = "first h1 tag"
-// document.querySelector("h1").innerText = "first h1 tag"
+// queryselectorAll --> select All
+let e = document.querySelectorAll("h1")
+console.log(e)
 
-// e.textContent = "h1 tag"
+// change text
+// innertext
+d.innerText = "change text with Js";
 
-e.innerHTML = "<i>first h1 tag</i>"
-// e.outerHTML = "<i>h1 with OuterHTML</i>"
-// e.outerHTML = "h1 with outertext"
+// outerHTML
+// d.outertext = "<h1>change text with outertext</h1>";
+
+// innerHTML
+d.innerHTML = "<i> change text with Js</i>";
+
+// outerHTML
+// d.outertext = "<i> change text style with Js </i>";
+
+// textContent -- use for change text
+d.textContent = "change text with textContent";
 
 // outer vs inner
 // innerText -- show text between tag -- ignore tag
-// outerText -- show text between tag -- remove tag
+// outerText -- shoe text between tag -- remove tag
 
 // innerHTML -- show text between too tag -- not tag
 // outerHTML -- show text with tag
 
-// ----------------------------------------------------------
-// Attribute Manipulation
+// -------------------------------------------
+// Attribute manipulation
 // getAttribute, setAttribute, removeAttribute
-
 let link = document.querySelector('a')
-console.dir(link)
-
-// console.log("get Attribute", info) // -- give value of href attribute
+console.log(link)
+console.log("get attribute", link.href) // -- give value of href attribute
 
 // link.href = "https://www.google.com/"
-// element.setAttribute("name", "change")
-
-link.setAttribute("href", "www.google.com")
+// element.setAttribute ("attribute_name", "change")
+link.setAttribute("href" , "www.google.com")
 let info = link.getAttribute("href");
-console.log("get Attribute", info) // -- give value
+console.log("get Attribute", info) // -- give value of href attribute
+
+// element.removeAttribute("name")
+link.removeAttribute("href")
+
+// ------------------------------------
+// dynamic DOM manipulation
+// createElement, appendChild, removeChild, prepend
+
+// createElement
+let box = document.createElement("div")
+box.textContent = "this is div tag create using Js"
+console.log(box)
+
+// append / prepend -- add element into HTML
+// append -- add element into last
+// document.body.append(box);
+// prepend -- add element into first
+document.body.prepend(box);
+
+// remove
+d.remove();
+
+// appendchild
+let outer_box = document.createElement("div");
+outer_box.classList.add("outer");
+document.body.append(outer_box);
+
+let inner_box = document.createElement("div");
+inner_box.classList.add("inner");
+outer_box.appendChild(inner_box);
+
+
+// append vs appendChild
+// remove vs removeChil
+
+// -----------------------------------------------
+// style updates via .style and classlist (add, remove, toggle)
+console.dir(d);
+d.style.backgroundcolor = "green";
+d.style.color = "white";
+d.style.padding = "20px";
+
+// classlist
+let card = document.querySelector(".card");
+// console.dir(card)
+card.classList.remove("card-style")
+
+// remove
+card.classList.remove("card")
+
+// toggle -- class --> available --> remove it
+//           class --> not available --> add it
+card.classList.toggle("card")
+
+
