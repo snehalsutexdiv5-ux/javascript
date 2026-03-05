2ea62158b9aeeb9df4d3db3f365e380547237fd6
snehalsutexdiv5-ux <snehalsutexdiv5@gmail.com>
2026-03-05 12:39:34 +0530

    this folder

---
+++ oops/script.js
@@ -0,0 +1,9 @@
+// Object Oriented Javascript
+// OOPS
+// dar vakhat blueprint banavo ke objectrit is kevo dekhase ane shu properties ane
+// methods hase, ane pachi tena upyog thi nava nava objects banvani sakiye chhiye tene j
+// kevay chhe Object Oriented Programming
+
+// blueprint(object kevo dekhase, object ma su method) ---> based on blueprint create objects
+// blueprint --> constructor
+// function based constructor ---> Constructor Function ---> function Name always start with Capital letter
